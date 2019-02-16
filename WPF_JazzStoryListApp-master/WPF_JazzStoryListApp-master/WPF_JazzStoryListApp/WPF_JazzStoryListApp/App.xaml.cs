3277682f94a8a01c10c1deba63fa59bbe9e8c9fd
chance221 <39056884+chance221@users.noreply.github.com>
2019-02-16 11:57:42 -0500

    Add files via upload

---
+++ WPF_JazzStoryListApp-master/WPF_JazzStoryListApp-master/WPF_JazzStoryListApp/WPF_JazzStoryListApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_JazzStoryListApp;
+using WPF_JazzStoryListApp.BusinessLayer;
+
+namespace WPF_JazzStoryListApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Here is where we create a new Business Layer object. 
+            // Before we are able to do this, we need to create a new 
+            // product business layer class that holds the fields, properties 
+            // and constructors that allow us to work with this class of objects
+
+            ProductBL productViewerBL = new ProductBL(); //If there are red squigilies itsmost likely because it is not linking to the class correctly
+        }
+    }
+}
---
+++ WPF_JazzStoryListApp-master/WPF_JazzStoryListApp-master/WPF_JazzStoryListApp/WPF_JazzStoryListApp/BusinessLayer/ProductBL.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_JazzStoryListApp.PresentationLayer;
+using WPF_JazzStoryListApp.Models;
+
+
+namespace WPF_JazzStoryListApp.BusinessLayer
+{
+    public class ProductBL //the access modifier needs to be public to allow other files to access this.
+    {
+        #region ENUMS
+
+        #endregion
+
+        #region FIELDS
+        //As we are currently in the Product Business Layer we need to create a spot in memory for BOTH the View and the Presenter as we need them to talk to each other 
+        //and access data to and from each other.
+
+        ProductWindowView _productWindowView; 
+        ProductWindowPresenter _productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+        // You know it is a constructor because it is a method that will have the exact same name of the class
+        public ProductBL()
+        {
+            //
+            // instantiate the view model and initialize the data set
+            //
+
+            _productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            //
+            // instantiate, set the data context, and show the Main Window
+            //
+
+            _productWindowView = new ProductWindowView();
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+        #endregion
+
+        #region METHODS
+    private Company GetCompanyData()
+    {
+        return new Company()
+        {
+            Name = "Jazz Story Software Inc.",
+            Address = "465 Smooth Blvd.",
+            City = "Detroit"
+        };
+    }
+
+    private Products GetProductData()
+        {
+            return new Products()
+            {
+                ArtistID = 1,
+                FirstName = "John",
+                LastName = "Coltrane",
+                ImageFileName = "coltrane.jpg",
+                Birthdate = DateTime.Parse("09-23-1926"),
+                SongName = "Bakai",
+                Album = "Coltrane",
+                YearProduced = 1957,
+                AliveOrDeceased = Products.AliveOrNot.deceased,
+                Gender = Products.GenderType.male,
+                SongFact = "Written by a friend, trumpeter and one-time roommate of Coltrane's named Calvin Massey, " +
+                "Bakai means \"City\" in Arabic The title was used in memory of Emmett Till, a 14-year-old black boy " +
+                "who was killed by older white men in the American South in 1955 for whisteling at a white woman. This song" +
+                " was the beginning of John Cltrane's connection to the civil rights movement."
+            };
+        }
+
+        
+
+        #endregion
+
+        #region EVENTS
+
+        #endregion
+
+    }
+}
